179ace692c51dabfd15ce2e7a12762ef3e40f805
leeroy79 <lee.borlace@gmail.com>
2018-02-23 06:18:45 +0800

    Predicting sample pics.

--- Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn_improved_1.py
+++ Deep_Learning_A_Z/Volume 1 - Supervised Deep Learning/Part 2 - Convolutional Neural Networks (CNN)/Section 8 - Building a CNN/cnn_improved_1.py
@@ -17,6 +17,11 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.layers import Dropout
+
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+
 
 # Initialising the CNN
 classifier = Sequential()
@@ -25,13 +30,13 @@ classifier = Sequential()
 
 # LB - 32 feature detectors - 3x3 each. 32 is a good place to start with. Input images are 64x64 with 3 channels (colour). Later we will make sure
 # the input images match this.
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Conv2D(64, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Adding a second convolutional layer
-classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(Conv2D(64, (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 # Step 3 - Flattening
@@ -39,7 +44,8 @@ classifier.add(Flatten())
 
 # Step 4 - Full connection
 #LB - good practice to choose a power of 2.
-classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 256, activation = 'relu'))
+classifier.add(Dropout(p = 0.1))
 classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 # Compiling the CNN
@@ -59,18 +65,18 @@ test_datagen = ImageDataGenerator(rescale = 1./255)
 
 training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                  target_size = (64, 64),
-                                                 batch_size = 32,
+                                                 batch_size = 64,
                                                  class_mode = 'binary')
 
 test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size = (64, 64),
-                                            batch_size = 32,
+                                            batch_size = 64,
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
                          steps_per_epoch = 8000,
-                         epochs = 25,
+                         epochs = 50,
                          validation_data = test_set,
                          validation_steps = 2000)
 
-classifier.save(filepath = "cnn.h5")                         
+classifier.save(filepath = "cnn_improved.h5")                         
